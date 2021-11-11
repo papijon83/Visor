5f92c784c9576c11fdbb16479705f1299ae99c70
jpalacig <papijon83@gmail.com>
2021-11-11 14:26:05 -0600

    modif tamaño iframe 2

--- resources/views/visor.blade.php
+++ resources/views/visor.blade.php
@@ -1,2 +1,2 @@
 
-<iframe src="https://docs.google.com/viewer?url=<?php echo e($ruta) ?>&embedded=true" width="1000" height="1000" style="border: none;"></iframe>
+<iframe src="https://docs.google.com/viewer?url=<?php echo e($ruta) ?>&embedded=true" width="1500" height="1500" style="border: none;"></iframe>
